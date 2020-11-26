56df831f824ae62eea9d8bee72ce81ae0174c7a6
psheborskiy <yury.psheborsky@ria.com>
2020-11-26 08:08:48 +0200

    Check Point

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const redis = require("redis");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const client = redis.createClient();
+  client.set("test_key", "my_value", redis.print);
+
   res.render('index', { title: 'Express' });
 });
 
